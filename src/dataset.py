815032cc64862b7900ba2c30caeefeeeb266efeb
madosaro <mathis24.dossantos@gmail.com>
2026-06-15 10:33:25 +0200

    MOD : dataset.py  - Made some modifications to correct the code

--- src/dataset.py
+++ src/dataset.py
@@ -25,24 +25,24 @@ class Nyudepth_png(Dataset):
         self.transform_color = transform_color
 
     def __len__(self):
-        return self.dataframe.size
+        return len(self.dataframe)
     
     def __getitem__(self, index):
         
-        #--- retrieve the files paths and convert images to np.arrays
-        rgb_file_path = os.path.join(self.dataset_path, self.df['rgb'].loc[index])
-        dep_file_path = os.path.join(self.dataset_path, self.df['dep'].loc[index])
+        #--- retrieve the files paths and image files   
+        rgb_file_path = os.path.join(self.dataset_path, self.dataframe['rgb'].iloc[index])
+        dep_file_path = os.path.join(self.dataset_path, self.dataframe['dep'].iloc[index])
 
-        rgb_data = np.array(im.open(rgb_file_path).convert('RGB'), dtype=np.float32) 
-        dep_data = np.array(im.open(dep_file_path), dtype=np.float32)
+        rgb_img = im.open(rgb_file_path).convert('RGB')
+        dep_img = im.open(dep_file_path)
 
         #--- resize the data for DPU
-        rgb_data = cv2.resize(rgb_data, (224, 224))
-        dep_data = cv2.resize(dep_data, (224, 224))
+        rgb_data = cv2.resize(np.array(rgb_img, dtype=np.float32), (224, 224))
+        dep_data = cv2.resize(np.array(dep_img, dtype=np.float32), (224, 224))
 
         #--- normalize the data
         rgb_data = np.transpose(rgb_data, (2, 0, 1)) #swap from (1, 1, 3) to (3, 1, 1)
-        rgb_data = (rgb_data - np.min(rgb_data))/((np.max(rgb_data) - np.min(rgb_data))) #normalize data values to the interval [0,1]
+        rgb_data = (rgb_data - np.min(rgb_data))/(np.max(rgb_data) - np.min(rgb_data) + 1e-8) #normalize data values to the interval [0,1]
         rgb_data = (rgb_data - 0.5) * 2.0 #shift data to the interval [-1:1]
         
             #for this part we know the data is encoded on 16bits,
@@ -50,24 +50,30 @@ class Nyudepth_png(Dataset):
             #between 0.7-10m measured in mm
             #based on the Nyudepth dataset informations
         dep_data = dep_data/1000.0 #convert in meters
+        dep_data = np.clip(dep_data, 0.7, 10.0) #clip data outside defined range
         dep_data = (dep_data - 0.7)/(10.0 - 0.7) #normalize data values to the interval [0:1]
-        dep_data = np.clip(dep_data, 0.0, 10.0) #clip data outside defined range
+        
 
         #--- equalize dep_data array dimensions with rgb_data array dimensions
         dep_data = np.expand_dims(dep_data, axis=0)
 
-        #--- combine dep_data and rgb_data in one array
-        data = np.append(rgb_data, dep_data, axis=0)
-        data = torch
-
+        #--- transform np.arrays to tensors
+        rgb_tensor = torch.from_numpy(rgb_data)
+        dep_tensor = torch.from_numpy(dep_data)
+       
+    
         #--- apply transforms
-        if self.transform_shape is not None:
-            data = self.transform_shape(data)
-        
-        if self.transform_2 is not None:
-            data[0:3] = self.transform_color(data[0:3])
+        if self.transform_color is not None:
+            rgb_tensor = self.transform_color(rgb_tensor)
 
-        return data[0:3], data[3].unsqueeze(dim=0)   # (chanels, Height, Width) and (depth, Height, Width)
+        if self.transform_shape is not None:
+            data_tensor = torch.cat([rgb_tensor, dep_tensor], dim=0)
+            data_tensor = self.transform_shape(data_tensor)
+            rgb_tensor = data_tensor[0:3]
+            dep_tensor = data_tensor[3:4]
+
+        
+        return rgb_tensor, dep_tensor   # (chanels, Height, Width) and (depth, Height, Width)
 
 
 
