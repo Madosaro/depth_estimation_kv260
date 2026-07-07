10ef0823a00f8372f0ff5ab5cf4047cf89c87f4a
madosaro <mathis24.dossantos@gmail.com>
2026-07-07 13:12:55 +0200

    MOD : dataset.py  -  removed cv2 dependancy

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,5 @@
 #dataset.py
 
-import os
-import cv2
 import torch
 import torchvision.transforms as tf
 import pandas as pd
@@ -34,17 +32,21 @@ class Nyudepth_png(Dataset):
         rgb_file_path = self.dataframe['rgb'].iloc[index]
         dep_file_path = self.dataframe['depth'].iloc[index]
 
-        rgb_img = Image.open(rgb_file_path).convert('RGB')
-        dep_img = Image.open(dep_file_path)
+        rgb_data = np.array(Image.open(rgb_file_path).convert('RGB'))[..., ::-1] #We load as rgb but permute channels since the images are in bgr
+        dep_data = np.array(Image.open(dep_file_path), dtype=np.float32)
 
-        #--- resize the data for DPU
-        rgb_data = cv2.resize(np.array(rgb_img, dtype=np.float32), (224, 224))
-        dep_data = cv2.resize(np.array(dep_img, dtype=np.float32), (224, 224))
+        rgb_data = np.array(
+            Image.fromarray(rgb_data).resize((224, 224), resample=Image.BILINEAR),
+            dtype=np.float32
+        )
+        dep_data = np.array(
+            Image.fromarray(dep_data).resize((224, 224), resample=Image.NEAREST),
+            dtype=np.float32
+        )
 
         #--- normalize the data
         rgb_data = np.transpose(rgb_data, (2, 0, 1)) #swap from (1, 1, 3) to (3, 1, 1)
         rgb_data = (rgb_data - np.min(rgb_data))/(np.max(rgb_data) - np.min(rgb_data) + 1e-8) #normalize data values to the interval [0,1]
-        rgb_data = (rgb_data - 0.5) * 2.0 #shift data to the interval [-1:1]
         
             #for this part we know the data is encoded on 16bits,
             #and we assume captured values for distances varying 
@@ -53,7 +55,7 @@ class Nyudepth_png(Dataset):
         dep_data = (dep_data*10)/(2**16 - 1) #convert in meters
         dep_data = np.clip(dep_data, 0.7, 10.0) #clip data outside defined range
         dep_data = (dep_data - 0.7)/(10.0 - 0.7) #normalize data values to the interval [0:1]
-        
+
 
         #--- equalize dep_data array dimensions with rgb_data array dimensions
         dep_data = np.expand_dims(dep_data, axis=0)
@@ -66,6 +68,9 @@ class Nyudepth_png(Dataset):
         #--- apply transforms
         if self.transform_color is not None:
             rgb_tensor = self.transform_color(rgb_tensor)
+
+        #--- end rgb normalisation by puttiing its values in the [-1:1]
+        rgb_tensor = (rgb_tensor - 0.5) * 2.0
 
         if self.transform_shape is not None:
             data_tensor = torch.cat([rgb_tensor, dep_tensor], dim=0)
