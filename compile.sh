8824a591fdf930b713956aed404365bdd7c4cd55
madosaro <mathis24.dossantos@gmail.com>
2026-07-14 15:37:32 +0200

    MOD : compile.sh  - Added cross vitis-ai versions compatibility

--- compile.sh
+++ compile.sh
@@ -17,14 +17,6 @@
 # Author: Mark Harvey, Xilinx Inc
 
 if [ $1 = kv260 ]; then
-    if [ "$2" = "vai_2.5" ]; then
-        COMPILED_DIR=compiled_vai_2.5
-    elif [ "$2" = "vai_1.4" ]; then
-        COMPILED_DIR=compiled_vai_1.4
-    else
-    echo "Unknown Vitis AI version: $2"
-    exit 1
-    fi
       ARCH=./arch.json
       TARGET=kv260
       echo "-----------------------------------------"
@@ -36,20 +28,23 @@ else
 fi
 
 MODEL_NAME=$3
-BUILD_DIR=./models/${MODEL_NAME}/build
+BUILD_DIR=./models/${MODEL_NAME}/build_$2
 QUANT_MODEL_PATH=${BUILD_DIR}/quant_model/UNet_int.xmodel
 
-mkdir -p ${BUILD_DIR}/${COMPILED_DIR}
+COMPILED_DIR=${BUILD_DIR}/compiled_model
+
+mkdir p ${COMPILED_DIR}
+    
 
 compile() {
   vai_c_xir \
   --xmodel      ${QUANT_MODEL_PATH} \
   --arch        $ARCH \
   --net_name    CNN_${TARGET} \
-  --output_dir  ${BUILD_DIR}/${COMPILED_DIR}
+  --output_dir  ${COMPILED_DIR}
 }
 
-compile 2>&1 | tee ${BUILD_DIR}/${COMPILED_DIR}/compile_$TARGET.log
+compile 2>&1 | tee ${COMPILED_DIR}/compile_$TARGET.log
 
 
 echo "-----------------------------------------"
